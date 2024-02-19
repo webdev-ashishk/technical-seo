dc29a12545f9b8898d9206e50536fbfd51c2ed99
webdev-ashishk <webdev.ashishk.com>
2024-02-20 04:19:39 +0530

    added google search console verification

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,26 @@
 const Contact = () => {
   return (
-    <div className="mt-16">
+    <div className="mt-16 contact">
       Contact
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
-      <h4>contact</h4>
+      <div className="overlay">
+        <button className="border-2 p-3 bg-purple-400 rounded-lg text-2xl">
+          overlay
+        </button>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        repudiandae ab, facere excepturi exercitationem ipsam quidem fugiat
+        laborum consequatur doloribus! Totam, exercitationem nobis aspernatur
+        vero ratione cupiditate delectus voluptatum doloremque, quaerat quisquam
+        ex. At optio minima debitis pariatur voluptatum. Dolorem ipsa, excepturi
+        minima iure ipsum quibusdam qui totam! Sequi illo ullam earum porro,
+        cupiditate quis necessitatibus iusto voluptatem magnam qui odio facere
+        reiciendis iure architecto, eaque fugit rerum culpa! Similique provident
+        explicabo cupiditate officiis dolorem fuga in, impedit ab unde
+        consectetur qui libero sit inventore exercitationem aut aperiam
+        blanditiis expedita tenetur deserunt veniam, distinctio incidunt. Modi
+        cumque et itaque repellendus?
+      </p>
     </div>
   );
 };
